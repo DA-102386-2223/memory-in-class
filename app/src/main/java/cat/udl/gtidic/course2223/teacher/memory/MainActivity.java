51e2bb244bced2851ad8d08d021d248ec4ef499c
albertrovira <albert@diphone.net>
2023-02-13 16:13:38 +0100

    initial commit

---
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/memory/MainActivity.java
@@ -0,0 +1,25 @@
+package cat.udl.gtidic.course2223.teacher.memory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        TODO
+
+//        Uri uri = Uri.parse(" https://www.campusigualada.udl.cat");
+//        Intent it = new Intent( Intent.ACTION_VIEW, uri);
+//        startActivity(it);
+
+    }
+}
