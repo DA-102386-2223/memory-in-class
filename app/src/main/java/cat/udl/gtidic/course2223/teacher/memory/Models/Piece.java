986932d420f28eaa59babd64732805e7d52fc5c0
albertrovira <albert@diphone.net>
2023-02-27 16:24:29 +0100

    Overriding toString method

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/memory/Models/Piece.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/memory/Models/Piece.java
@@ -2,4 +2,9 @@ package cat.udl.gtidic.course2223.teacher.memory.Models;
 
 public class Piece {
     String value;
+
+    @Override
+    public String toString(){
+        return "Peça amb valor: " + value;
+    }
 }
