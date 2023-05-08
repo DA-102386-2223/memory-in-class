d633cb6ea3a8bad8bb3c64114aa8ca5252dc3487
diphone-net <43610108+diphone-net@users.noreply.github.com>
2023-05-08 17:15:15 +0200

    RV jumpToGame v1

--- app/src/main/java/cat/udl/gtidic/course2223/teacher/memory/helpers/adapters/MultiplayerMatchViewHolder.java
+++ app/src/main/java/cat/udl/gtidic/course2223/teacher/memory/helpers/adapters/MultiplayerMatchViewHolder.java
@@ -1,5 +1,7 @@
 package cat.udl.gtidic.course2223.teacher.memory.helpers.adapters;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -7,7 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import cat.udl.gtidic.course2223.teacher.memory.R;
+import cat.udl.gtidic.course2223.teacher.memory.models.Game.Game;
 import cat.udl.gtidic.course2223.teacher.memory.models.MultiplayerMatch;
+import cat.udl.gtidic.course2223.teacher.memory.views.GameActivity;
 
 class MultiplayerMatchViewHolder extends RecyclerView.ViewHolder{
 
@@ -17,6 +21,20 @@ class MultiplayerMatchViewHolder extends RecyclerView.ViewHolder{
         super(itemView);
         this.itemView = itemView;
         this.username = itemView.findViewById(R.id.rv_item_username);
+        itemView.findViewById(R.id.item_rv).setOnClickListener(v -> jumpToGame());
+    }
+
+    private void jumpToGame() {
+
+        String firebaseKey = username.getText().toString();
+
+        Activity fake = (Activity)itemView.getContext();
+        Intent i = new Intent(fake, GameActivity.class);
+        i.putExtra(Game.MULTIPLAYER_KEY, Game.MULTIPLAYER_TYPE_CONNECT);
+        i.putExtra(Game.MULTIPLAYER_GAME_KEY, firebaseKey);
+        fake.startActivity(i);
+
+//        startActivity(i);
     }
 
     public void render(MultiplayerMatch mm) {
